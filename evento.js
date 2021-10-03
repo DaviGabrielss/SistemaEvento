4370bf84a6262995015c1d918dce9284825ed294
DaviGabrielss <88987871+DaviGabrielss@users.noreply.github.com>
2021-10-03 17:30:05 -0300

    Create evento.js

---
+++ evento.js
@@ -0,0 +1,27 @@
+//Condições para se cadastrar
+var DataAtual = new Date()
+
+var DataEvento = new Date("27 de Março de 2022")
+
+if (DataAtual.lenght<=DataEvento.lenght) {
+//É possivível efetuar o cadastro
+   console.log("É possivel continuar o cadastro")
+}else if (dataAtual>DataEvento) {
+    console.log("Não é possivel continuar o cadastro")
+}
+//Idade limite tem que ser maior de 18 anos.
+
+let idade = 18;
+if (idade.length > 18) {
+    console.log("Maior de 18, cadastro em andamento!")   
+
+}else if(idade<18) 
+     console.log("Menor de 18, infelizmente cadastro indisponivel.")
+
+    var Participantes = [ "1", "2", "3", "50"]
+     
+    if(Participantes.lenght<100){
+        console.log("Cadastro efetuado com sucesso")
+    }else{ 
+        console.log("Cadastro indisponivel, pois excedeu o numero limite de pessoas cadastradas")
+    }
